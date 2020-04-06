41934dff59442de1aa1a97729b6f4f7871662b7d
EFlores <eddyflores@icloud.com>
2020-04-06 14:14:25 -0400

    completed skeleton added static root page

---
+++ frontend/gigbrite.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDom from "react-dom";
+
+document.addEventListener(`DOMContentLoaded`, () => {
+    const root = document.getElementById(`root`);
+    ReactDom.render(<h1>Welcome to gigbrite</h1>, root);
+});
