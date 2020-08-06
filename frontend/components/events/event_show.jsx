7df58df4bd96e45954807a2cecbc14e72b8fd492
EFlores <eddyflores@icloud.com>
2020-08-06 13:28:15 -0400

    add event show css styling.

--- frontend/components/events/event_show.jsx
+++ frontend/components/events/event_show.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import NavContainer from '../nav/nav_container';
 class EventShow extends React.Component {
     constructor(props) {
         super(props);
@@ -16,9 +16,55 @@ class EventShow extends React.Component {
     }
 
     render() {
-
+        let event = this.props.event;
+        let picture = "";
+        if (event.title == "Aunt May Bday") {
+            picture = window.MaybdayURL
+        } else if (event.title == "Gamma Ray Ted Talk") {
+            picture = window.TedURL
+        } else if (event.title == "July 4th Party") {
+            picture = window.CaptainURL
+        } else if (event.title == "stark expo") {
+            picture = window.StarkURL
+        } else if (event.title == "Baxter Reopening") {
+            picture = window.BaxterURL
+        } else if (event.title == "App Academy Grad Party!") {
+            picture = window.AppacademyURL
+        }
+        
         return (
-        <p>{this.props.event.title}</p>
+            <>
+                <header>
+                    <NavContainer />
+                </header>
+                        
+
+                            
+                    <div className="event-show-box">
+                        <div className="event-show-image-detail">
+                            <div className="event-image">
+                                <img src={picture} alt=""/>
+                            </div>
+                            <div className="event-details">
+                                <h1>{event.title}</h1>
+                            </div>
+                        </div>
+                        <div className="event-summary">
+                            <div className="event-info-details">
+                                <p>{event.description}</p>
+                            </div>
+                            <div className="event-time-info">
+                                <p>{event.start_time}</p>
+                            </div>
+                        </div>
+                    </div>
+                        
+                <footer>
+                    <img src={FooterURL} alt="" />
+                </footer>
+                <br />
+
+            </>
         )
        
     }
